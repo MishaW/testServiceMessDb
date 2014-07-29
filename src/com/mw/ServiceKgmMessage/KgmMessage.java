9ccedc5c435a39a99d12fa752b29c411b1e9603e
MishaW <white701wind@gmail.com>
2014-07-30 01:53:11 +0400

    Реализована логика обновления сообщений, и уведомлений

--- src/com/mw/ServiceKgmMessage/KgmMessage.java
+++ src/com/mw/ServiceKgmMessage/KgmMessage.java
@@ -9,8 +9,11 @@ import android.os.IBinder;
 import android.os.PowerManager;
 import android.util.Log;
 import com.mw.kgmspread.AEvent;
+import com.mw.kgmspread.AEventBroadcastReceiver;
+import com.mw.kgmspread.Event;
 import com.mw.kgmspread.Subscriber;
-import com.mw.testServiceMessDb.MyActivity;
+
+import java.util.*;
 
 /**
  * Created by mishaw on 27.07.14.
@@ -21,13 +24,16 @@ public class KgmMessage extends Service implements Subscriber {
     private ServiceConnection serviceConnection;
     private Intent intentKgmAArm;
     private AEventBroadcastReceiver aEventBroadcastReceiver;
-    private BroadcastReceiver dataBroadcastReceiver;
     private PowerManager powerManager;
     private PowerManager.WakeLock wakeLock;
     private NotificationManager notificationManager;
     private final String LOG_TAG = "KgmAArm::";
-    private final String TAG_WAKE_LOCK = "Service MessageInDb";
-    public final String BROADCAST_ACTION_ = "com.mw.testServise.intent.action.AEVENT";
+    private final String TAG_WAKE_LOCK = "Service KgmMessage";
+    private final String BROADCAST_ACTION_DATA = "com.mw.ServiceKgmMessage.intent.action.DATA";
+    private final String COUNT_MESSAGE = "count_message";
+    boolean flagSendNotification;
+
+    private HashMap<Event, HashMap<Integer, Long>> dataMessage;
 
     @Override
     public void onCreate() {
@@ -47,19 +53,12 @@ public class KgmMessage extends Service implements Subscriber {
                 flagConnectService = false;
             }
         };
-
         aEventBroadcastReceiver = new AEventBroadcastReceiver(this);
-        dataBroadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-
-            }
-        };
-
-
         powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
         wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG_WAKE_LOCK);
+        dataMessage = new HashMap<Event, HashMap<Integer, Long>>();
         notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        flagSendNotification = false;
     }
 
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -82,23 +81,75 @@ public class KgmMessage extends Service implements Subscriber {
 
     @Override
     public IBinder onBind(Intent intent) {
+        flagSendNotification = false;
+        sendUpdate(null);
         return null;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        flagSendNotification = true;
+        return true;
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+        flagSendNotification = false;
+        sendUpdate(null);
     }
 
     @Override
     public void setEvent(AEvent aEvent) {
         Log.d(LOG_TAG, aEvent.toString());
-//        dbMessageHelper.dbOpen(true);
-//        if ( dbMessageHelper.isSushestvuet(aEvent) ){
-//            if ( aEvent.value == 0 )
-//                dbMessageHelper.delete(aEvent);
-//        } else {
-//            if ( aEvent.value == 1 ){
-//                dbMessageHelper.insertAEvent(aEvent, "ZAGLUSHKA");
-//                sendNotification(aEvent.toString());
-//            }
-//        }
-//        dbMessageHelper.dbClose();
+
+        HashMap<Integer, Long> tmpData;
+        if (aEvent.event != Event.NO_MESSAGE){
+            if (dataMessage.containsKey(aEvent.event)) {
+                tmpData = dataMessage.get(aEvent.event);
+            } else {
+                tmpData = new HashMap<Integer, Long>();
+                dataMessage.put(aEvent.event, tmpData);
+            }
+
+            if (tmpData.containsKey(aEvent.number)){
+                if (aEvent.value == 0){
+                    tmpData.remove(aEvent.number);
+                }
+            } else {
+                if (aEvent.value != 0){
+                    tmpData.put(aEvent.number, aEvent.value);
+                    sendUpdate(aEvent);
+                }
+            }
+        }
+    }
+
+    private void sendUpdate(AEvent aEvent) {
+        if (flagSendNotification){
+            sendNotification(aEvent.toString());
+        } else {
+            Intent intentDataMessage = new Intent(BROADCAST_ACTION_DATA);
+            int countMessage = 0;
+            HashMap<Integer, Long> tmpData;
+            Set<Event> setEvents = dataMessage.keySet();
+            for (Event event : setEvents) {
+                tmpData = dataMessage.get(event);
+                Set<Integer> setNumbers = tmpData.keySet();
+                for (Integer number : setNumbers) {
+                    long value = tmpData.get(number);
+
+                    intentDataMessage.putExtra(String.valueOf(countMessage), getMessage(event, number, value));
+                    ++countMessage;
+                }
+            }
+            intentDataMessage.putExtra(COUNT_MESSAGE, countMessage);
+            sendBroadcast(intentDataMessage);
+        }
+    }
+
+    private String getMessage(Event event, int number, long value) {
+        return "event=" + event.toString() + " number = " + String.valueOf(number) + " value=" + String.valueOf(value);
     }
 
     private void sendNotification(String s){
--- src/com/mw/ServiceKgmMessage/MyActivity.java
+++ src/com/mw/ServiceKgmMessage/MyActivity.java
@@ -1,52 +1,74 @@
 package com.mw.ServiceKgmMessage;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.database.Cursor;
+import android.content.*;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
-import com.mw.ServiceKgmMessage.KgmMessage;
-import dbmessage.DBMessageHelper;
 
 public class MyActivity extends Activity implements AdapterView.OnItemClickListener {
     /**
      * Called when the activity is first created.
      */
-    Button button;
-    ListView listView;
-    SimpleCursorAdapter simpleCursorAdapter;
-    Cursor cursor;
-    Intent intent;
-    DBMessageHelper dbMessageHelper;
+//    Button button;
+//    ListView listView;
+    BroadcastReceiver broadcastReceiver;
+    ServiceConnection serviceConnectionKgmMessage;
+    Intent intentKgmMessage;
+    private final String FLAG_NOTIFICATION = "flag_notification";
     final String LOG_TAG = "KgmAArm";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int count = intent.getIntExtra("count_message", 0);
+                for(int i=0; i<count; ++i){
+                    System.out.println("MessageInDb ----"+intent.getStringExtra(String.valueOf(i)));
+                }
+            }
+        };
 
-//        dbMessageHelper = new DBMessageHelper(this);
-//        dbMessageHelper.dbOpen(true);
-//        cursor = dbMessageHelper.getAllMessage();
-//        startManagingCursor(cursor);
+        intentKgmMessage = new Intent("com.mw.ServiceKgmMessage.KgmMessage");
+        serviceConnectionKgmMessage = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+
+            }
+        };
+
 //
 //        String[] from = new String[] { dbMessageHelper.FIELD_MESSAGE, dbMessageHelper.FIELD_TYPE };
 //        int[] to = new int[] { R.id.tvMessage, R.id.tvType };
-//
-//        if (cursor != null){
 //            simpleCursorAdapter = new SimpleCursorAdapter(this, R.layout.item_message, cursor, from, to);
 //            listView = (ListView) findViewById(R.id.listView);
 //            listView.setAdapter(simpleCursorAdapter);
 //            listView.setOnItemClickListener(this);
-//        }
-//        dbMessageHelper.dbClose();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        registerReceiver(broadcastReceiver, new IntentFilter("com.mw.ServiceKgmMessage.intent.action.DATA"));
+        intentKgmMessage.putExtra(FLAG_NOTIFICATION, false);
+        bindService(intentKgmMessage, serviceConnectionKgmMessage, 0);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        unbindService(serviceConnectionKgmMessage);
+        unregisterReceiver(broadcastReceiver);
     }
 
     public void onStartServiceKgmAArm(View v){
@@ -64,12 +86,6 @@ public class MyActivity extends Activity implements AdapterView.OnItemClickListe
     public void onStopServiceMessageInDb(View v){
         stopService(new Intent(this, KgmMessage.class));
     }
-
-    public void onNotification(View v){
-
-    }
-
-    private void updateListMessage(){}
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
